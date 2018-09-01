98211c249fae1c233ed3a025668186c612aefae4
Felipe Abreu <coisa.cs@gmail.com>
2018-08-31 21:54:51 -0300

    We all like tests! :)

---
+++ tests/LeagueContainerTest.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * @author Felipe Sayão Lobato Abreu <github@felipeabreu.com.br>
+ * @package CoiSA\Monolog\Container\ConfigProvider
+ */
+
+declare(strict_types=1);
+
+namespace CoiSA\Monolog\Test;
+
+use CoiSA\Monolog\Container\ServiceProvider\LeagueServiceProvider;
+use League\Container\Container;
+use Psr\Container\ContainerInterface;
+
+/**
+ * Class LeagueContainerTest
+ *
+ * @package CoiSA\Monolog\Test
+ */
+class LeagueContainerTest extends ContainerTest
+{
+    /**
+     * @return ContainerInterface
+     */
+    public function getContainer(): ContainerInterface
+    {
+        $container = new Container();
+        $container->addServiceProvider(new LeagueServiceProvider());
+
+        return $container;
+    }
+}
