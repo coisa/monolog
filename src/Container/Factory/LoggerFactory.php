3c7ab3637aafb04f523f62b552f362bf3846d4d0
Felipe Sayão Lobato Abreu <github@felipeabreu.com.br>
2019-04-15 11:57:45 -0300

    PHP CS Fix

--- src/Container/Factory/LoggerFactory.php
+++ src/Container/Factory/LoggerFactory.php
@@ -10,14 +10,6 @@
  */
 
 declare(strict_types=1);
-/*
- * This file is part of coisa/monolog.
- *
- * (c) Felipe Sayão Lobato Abreu <github@felipeabreu.com.br>
- *
- * This source file is subject to the Apache v2.0 license that is bundled
- * with this source code in the file LICENSE.
- */
 
 namespace CoiSA\Monolog\Container\Factory;
 
